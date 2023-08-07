2772b4d95efc521e45b26dde0e0769db21c5d2d0
Justin <orochitachi13@gmail.com>
2023-08-07 20:41:15 +1000

    added Exception catcher to continue script even after any exception. Error status will be noted in Google Sheets.

--- Immi Status Checker.py
+++ Immi Status Checker.py
@@ -1,96 +1,119 @@
 from multiprocessing.connection import wait
 import pandas as pd
-import openpyxl
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-from openpyxl import Workbook
 
-wb = openpyxl.load_workbook("../Immi Credentials.xlsx", data_only=True)
-ws = wb["Eduyoung"]
+import json
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+import gspread
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
+from datetime import datetime
+
+from selenium.webdriver.chrome.service import Service
+service = Service()
+
+with open('../../Json/keys.json') as f:
+    config = json.load(f)
+
+sheets_key = config['sheets_key']
 
 # load usernames
 
-def new_func(ws):
-    usernames = []
-    for user in ws["D"]:
-        if user.value == None:
-            continue
-        else: usernames.append(user.value)
+class Database:
+    def __init__(self):
+        
+        SERVICE_ACCOUNT_FILE = "../../Json/keys.json"
+        SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-    passwords = []
-    for password in ws["E"]:
-        if password.value == None:
-            continue
-        else: passwords.append(password.value)
+        self.credentials = None
+        self.credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+        self.clients_list_sheet = sheets_key
+        service = build("sheets", "v4", credentials=self.credentials)
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=self.clients_list_sheet, range="Immi Credentials").execute()
+        values = result.get('values', [])
+        self.database = pd.DataFrame(values)
+        self.database.columns = ["Date Lodged",	"Name",	"Expiry", "Username", "Password", "Medical Exam Date", "Status", "Current Status Date", "Previous Status Date"]
 
-    current_status = []
-    for status in ws["G"]:
-        if status.value == None:
-            continue
-        else: current_status.append(status.value)
-    return usernames,passwords,current_status
+    def update_sheet(self, database):
+        gc = gspread.authorize(self.credentials)
+        gs = gc.open_by_key(self.clients_list_sheet)
+        payment_plan_worksheet = gs.worksheet('Immi Credentials')
+        set_with_dataframe(worksheet=payment_plan_worksheet, dataframe=database, include_index=False,
+        include_column_header=False, resize=True)
 
-usernames, passwords, current_status = new_func(ws)
+data = Database()
 
 options = Options()
-options.headless = True
-# driver = webdriver.Chrome("/Applications/Google\ Chrome.app/Contents/MacOS/chromedriver", options=options)
-driver = webdriver.Chrome(options=options)
+# options.headless = True
+
+driver = webdriver.Chrome('/Users/justinbilao/Downloads/chromedriver-mac-arm64/chromedriver', options=options)
  
-for x in range(1, len(usernames)):
+def main():
 
-    # Skip if Application Status is already Finalised
-    if current_status[x] != "Finalised":
-        # Login
-        driver.get("https://online.immi.gov.au/ola/app")
-        driver.find_element("name", "username").send_keys(usernames[x])
-        driver.find_element("name", "password").send_keys(passwords[x])
-        driver.find_element("name", "login").send_keys(Keys.ENTER)
-        driver.find_element("name", "continue").send_keys(Keys.ENTER)
-        
-        # Copy Status from Website and Paste to Excel file
-        def update_status():
-            status_wait = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/form/section/div/div/div[3]/div/div[2]/div/div/div[2]/div/div[1]/div/div[2]/div/p/strong")))
-            global status
-            status = driver.find_element("xpath", "/html/body/form/section/div/div/div[3]/div/div[2]/div/div/div[2]/div/div[1]/div/div[2]/div/p/strong").text
-            status_cell = "G" + str(x + 2)
-            ws[status_cell] = status
+    for x in range(1, len(data.database["Username"])):
 
-        update_status()
-        
-        # Copy Last Update Date and Paste to Excel File 
-        
-        def last_update_date():
-            last_update_date_cell_wait = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/form/section/div/div/div[3]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/div/div/time")))
-            global last_update_date_cell
-            last_update_date_cell = driver.find_element("xpath", "/html/body/form/section/div/div/div[3]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/div/div/time").text
-            if last_update_date_cell == "":
+        # Skip if Application Status is already Finalised
+        if data.database.at[x,"Status"] != "Finalised":
+            # Login
+            try:
+                driver.get("https://online.immi.gov.au/ola/app")
+                driver.find_element("name", "username").clear()
+                driver.find_element("name", "username").send_keys(data.database.at[x,"Username"])
+                driver.find_element("name", "password").send_keys(data.database.at[x,"Password"])
+                driver.find_element("name", "login").send_keys(Keys.ENTER)
+                
+                driver.find_element("name", "continue").send_keys(Keys.ENTER)
+            
+                def update_status():
+                    status_wait = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/form/section/div/div/div[3]/div/div[2]/div/div/div[2]/div/div[1]/div/div[2]/div/p/strong")))
+                    global status
+                    status = driver.find_element("xpath", "/html/body/form/section/div/div/div[3]/div/div[2]/div/div/div[2]/div/div[1]/div/div[2]/div/p/strong").text
+                    data.database.at[x, "Status"] = status
+
+                update_status()
+
+                # Copy Last Update Date and Paste to Excel File 
+
+                def last_update_date():
+                    global last_update_date_cell
+                    last_update_date_cell = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/form/section/div/div/div[3]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/div/div/time"))).text
+                    # last_update_date_cell = driver.find_element("xpath", "/html/body/form/section/div/div/div[3]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/div/div/time").text
+
+                    if last_update_date_cell == "":
+                        last_update_date() # recurse until Update Date is visible/detected
+
+                    print("New Update:", last_update_date_cell)
+
+                    # if there is a new update, the current update will be moved to the next cell. the new update will be at the current cell.
+                    if data.database.at[x, "Current Status Date"] != last_update_date_cell:
+                        data.database.at[x, "Previous Status Date"] = str(data.database.at[x, "Current Status Date"])
+                        print("New Update:", last_update_date_cell)
+
+                    data.database.at[x, "Current Status Date"] = str(last_update_date_cell)
+
                 last_update_date()
-            
-            update_dates = "H" + str(x + 2)
-            previous_update = "I" + str(x + 2)
-            
-            # if there is a new update, current update will be moved to next cell. new update will be at current cell.
-            if ws[update_dates] != last_update_date_cell:
-                ws[previous_update] = ws[update_dates].value
-                print("New Update:", last_update_date_cell)
 
-            ws[update_dates] = last_update_date_cell
-        
-        last_update_date()
-        
-        # Print status to terminal for each user
-        user = str(usernames[x])
-        print("%s is %s" % (user, status))
+                # Print status to the terminal for each user
+                user = str(data.database.at[x, "Username"])
+                print("%s is %s" % (user, status))
 
-        # Logout and Save Excel File
-        wb.save("../Immi Credentials.xlsx")
-        driver.find_element("xpath", "/html/body/form/header/div/div/ol/li[3]/button").click()
-        driver.find_element("xpath", "/html/body/header/div/ul/li/div/a").click()
+                # Logout
+                driver.find_element("xpath", "/html/body/form/header/div/div/ol/li[3]/button").click()
+                driver.find_element("xpath", "/html/body/header/div/ul/li/div/a").click()
 
-driver.quit()
+            except Exception as e:
+                data.database.at[x, "Status"] = "Error"
+                
+    data.update_sheet(data.database)
+    driver.quit()
+
+main()
